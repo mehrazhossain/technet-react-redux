63cdf24e1fbc0f615614262f55eac24c39b5b6b6
Mehraz <mehrazhossain54@gmail.com>
2023-07-08 21:18:02 +0600

    added product filters using redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
+import productReducers from './features/products/productSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartReducer,
+    product: productReducers,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
