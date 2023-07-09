2655249b0445babeac7e7020a016322f74c4d623
Mehraz <mehrazhossain54@gmail.com>
2023-07-09 22:16:14 +0600

    Making first get request using query

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
 import productReducers from './features/products/productSlice';
+import { api } from './api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     product: productReducers,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
