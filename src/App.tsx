047cf09e5290ae8c9f27b9aa4d1cd61a2c0cd8f9
Mehraz <mehrazhossain54@gmail.com>
2023-07-12 13:26:18 +0600

    Handling-user-persistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { Toaster } from './components/ui/Toaster';
 import MainLayout from './layouts/MainLayout';
+import { setLoading, setUser } from './redux/features/user/userSlice';
+import { useAppDispatch } from './redux/hooks';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setLoading(true));
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user.email));
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    });
+  }, []);
+
   return (
     <div>
       <Toaster />
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,23 +13,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
-/*  
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyBrFAHoRo8RGFZEPPMp8AJV2bcMdWWq2M0',
-  authDomain: 'technet-85062.firebaseapp.com',
-  projectId: 'technet-85062',
-  storageBucket: 'technet-85062.appspot.com',
-  messagingSenderId: '217380423981',
-  appId: '1:217380423981:web:4c804abce8e8b7db22aba4',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-*/
